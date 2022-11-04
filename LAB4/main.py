7400299eab14c85fe2a9cd87ebdc8cfa07087831
raresjunica <79074292+raresjunica@users.noreply.github.com>
2022-11-04 13:26:34 +0200

    LAB4

--- LAB4/main.py
+++ LAB4/main.py
@@ -1,24 +1,50 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import pymc3 as pm
-import arviz as az
 from numpy import random
 
-model = pm.Model()
-x = np.random.poisson(20, 1)
-alpha = 14
+import arviz as az
+import matplotlib.pyplot as plt
 
-with model:
-    y = pm.Normal('Timp', sigma=0.5, mu=1)
-    z = pm.Exponential('Comanda', 1 / alpha)
-    trace = pm.sample(x[0], chains=1)
+import numpy as np
+import pymc3 as pm
+import pandas as pd
 
-dictionary = {
-    'timp': trace['Timp'].tolist(),
-    'comanda': trace['Comanda'].tolist()
-}
-df = pd.DataFrame(dictionary)
+df = pd.read_csv('data.csv')
 
-az.plot_posterior(trace)
+plt.scatter(df.ppvt, df.momage, color="b", s=100)
+plt.xticks(rotation=25)
+plt.xlabel('PPVT')
+plt.ylabel('Mom Aage')
+plt.title('PPVT Report', size=20)
+
 plt.show()
+
+average_age = 0
+average_ppvt = 0
+average_educ_cat = 0
+for age in df.momage:
+    average_age = average_age + age
+for ppvt in df.ppvt:
+    average_ppvt = average_ppvt + ppvt
+for educ in df.educ_cat:
+    average_educ_cat = average_educ_cat + educ
+
+
+average_age = average_age / df.index.stop
+average_ppvt = average_ppvt / df.index.stop
+average_educ_cat = average_educ_cat / df.index.stop
+
+
+with pm.Model() as model_g:
+    mom_age = pm.Normal('mom_age', mu=average_age, sd=1)
+    ppvt = pm.Normal('ppvt', mu=average_ppvt, sd=1)
+    educ_cat = pm.Normal('educ_cat', mu=average_educ_cat, sd=1)
+
+map_estimate = pm.find_MAP(model=model_g)
+print(map_estimate)
+
+_, ax = plt.subplots(1, 2, figsize=(8, 4))
+ax[0].plot(mom_age, ppvt, 'CO.')
+ax[0].set_xlabel('Mom Age')
+ax[0].set_ylabel('PPVT')
+plt.tight_layout()
+plt.show()
+
