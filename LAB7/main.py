d8365298122e6439cff04b45d0a01a73dcd6f3e7
raresjunica <79074292+raresjunica@users.noreply.github.com>
2022-11-22 20:48:30 +0200

    LAB7  completare

--- LAB7/main.py
+++ LAB7/main.py
@@ -3,40 +3,45 @@ import numpy as np
 import pandas as pd
 import pymc3 as pm
 import arviz as az
+import math
+from scipy import stats
 
-np.random.seed(1)
 if __name__ == '__main__':
-    data = pd.read_csv('Prices.csv')
-
-    price = data['Price'].values
-    speed = data['Speed'].values
-    hardDrive = data['HardDrive'].values
-    ram = data['Ram'].values
-    premium = data['Premium'].values
-
-    # fig, axes = plt.subplots(2, 2, sharex=False, figsize=(10, 8))
-    # axes[0, 0].scatter(speed, price, alpha=0.6)
-    # axes[0, 1].scatter(hardDrive, price, alpha=0.6)
-    # axes[1, 0].scatter(ram, price, alpha=0.6)
-    # axes[1, 1].scatter(premium, price, alpha=0.6)
-    # axes[0, 0].set_ylabel("Price")
-    # axes[0, 0].set_xlabel("Speed")
-    # axes[0, 1].set_xlabel("HardDrive")
-    # axes[1, 0].set_xlabel("Ram")
-    # axes[1, 1].set_xlabel("Premium")
-    # plt.savefig('price_correlations.png')
-
-    x = np.array(data["Speed"], np.log(data["HardDrive"]))
-    x_mean = x.mean(axis=0, keepdims=True)
-    x_centered = x - x_mean
+    data = pd.read_csv("Prices.csv")
+    Price = data['Price'].values
+    Speed = data['Speed'].values
+    HardDrive = data['HardDrive'].values
+    Ram = data['Ram'].values
+    Premium = data['Premium'].values
     with pm.Model() as model:
-        a_tmp = pm.Normal('a_tmp', mu=0, sd=10)
-        beta_1 = pm.Normal('beta_1', mu=0, sigma=10)
-        beta_2 = pm.Normal('beta_2', mu=0, sigma=10)
-        epsilon = pm.HalfCauchy('epsilon', 5)
-        miu = a_tmp + speed*beta_1 + hardDrive*beta_2
-        a = pm.Deterministic('a', a_tmp - pm.math.dot(x_mean, beta_1))
-        y_pre = pm.Normal('y_pre', mu=miu, sd=epsilon, observed=price)
-        step = pm.Slice()
-        step = pm.Metropolis()
-        idata_model = pm.sample(2000, step=step, tune=2000, return_inferencedata=True)
+        alpha = pm.Normal('alpha', mu=0, sd=1)
+        beta1 = pm.Normal('beta1', mu=0, sd=1)
+        beta2 = pm.Normal('beta2', mu=0, sd=1)
+        sigma = pm.HalfNormal('sigma', sd=1)
+        mu = pm.Deterministic('mu', alpha + beta1 * Speed + beta2 * [math.log(i) for i in HardDrive])
+        Price_obs = pm.Normal('price_obs', mu=mu, sigma=sigma, observed=Price)
+        trace = pm.sample(5000, tune=1000)
+        predictive = pm.sample_posterior_predictive(trace, var_names=["mu"], samples=5000)
+    az.plot_posterior(
+        {"beta1": trace['beta1'], "beta2": trace['beta2']},
+        hdi_prob=0.95)
+    plt.show()
+    az.plot_posterior(
+        {"pret asteptat": trace['mu']},
+        hdi_prob=0.9)
+    with pm.Model() as model:
+        alpha = pm.Normal('alpha', mu=0, sd=1)
+        beta = pm.Normal('beta', mu=0, sd=1)
+        sigma = pm.HalfNormal('sigma', sd=1)
+        mu = pm.Deterministic('mu', alpha + beta * [0 if i == "no" else 1 for i in Premium])
+        Price_obs = pm.Normal('price_obs', mu=mu, sigma=sigma, observed=Price)
+        trace = pm.sample(5000, tune=1000)
+    az.plot_posterior(
+        {"premium_beta": trace['beta']})
+    plt.show()
+
+# Media beta ~0 => producatorul premium are impact prea mare
+
+# ex3
+# Avand in vedere deviatia standard, cat si faptul ca beta1 si beta2 sunt diferite de 0 putem spune ca sunt
+# indicatori utili ai pretului
